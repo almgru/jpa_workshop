5be9c9f1893091286ebb0db75beefbcf0a10a9ab
Daniel Alm Grundström <daniel.alm.grundstrom@protonmail.com>
2021-03-10 10:01:17 +0100

    Adds entities

---
+++ src/main/java/se/lexicon/almgru/jpa_workshop/entity/AppUser.java
@@ -0,0 +1,95 @@
+package se.lexicon.almgru.jpa_workshop.entity;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
+public class AppUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer appUserId;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+    private LocalDate regDate;
+
+    @OneToOne(
+            cascade = {CascadeType.DETACH, CascadeType.REFRESH, CascadeType.PERSIST, CascadeType.MERGE},
+            fetch = FetchType.LAZY
+    )
+    @JoinColumn(name = "details_id")
+    private Details userDetails;
+
+    public AppUser(Integer appUserId, String username, String password, LocalDate regDate, Details userDetails) {
+        this.appUserId = appUserId;
+        this.username = username;
+        this.password = password;
+        this.regDate = regDate;
+        this.userDetails = userDetails;
+    }
+
+    public AppUser() {
+    }
+
+    public Integer getAppUserId() {
+        return appUserId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDate getRegDate() {
+        return regDate;
+    }
+
+    public void setRegDate(LocalDate regDate) {
+        this.regDate = regDate;
+    }
+
+    public Details getUserDetails() {
+        return userDetails;
+    }
+
+    public void setUserDetails(Details userDetails) {
+        this.userDetails = userDetails;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AppUser appUser = (AppUser) o;
+        return Objects.equals(appUserId, appUser.appUserId) && Objects.equals(username, appUser.username) && Objects.equals(password, appUser.password) && Objects.equals(regDate, appUser.regDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(appUserId, username, password, regDate);
+    }
+
+    @Override
+    public String toString() {
+        return "AppUser{" +
+                "appUserId=" + appUserId +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", regDate=" + regDate +
+                '}';
+    }
+}
