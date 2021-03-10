1ef5d03e6e29fdaa85e1a22504d6f5701f863b5a
Daniel Alm Grundström <daniel.alm.grundstrom@protonmail.com>
2021-03-10 12:39:27 +0100

    Adds book and book loan entities

---
+++ src/main/java/se/lexicon/almgru/jpa_workshop/entity/Book.java
@@ -0,0 +1,76 @@
+package se.lexicon.almgru.jpa_workshop.entity;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer bookId;
+
+    private String isbn;
+    private String title;
+    private int maxLoanDays;
+
+    public Book(Integer bookId, String isbn, String title, int maxLoanDays) {
+        this.bookId = bookId;
+        this.isbn = isbn;
+        this.title = title;
+        this.maxLoanDays = maxLoanDays;
+    }
+
+    public Book() {
+    }
+
+    public Integer getBookId() {
+        return bookId;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getMaxLoanDays() {
+        return maxLoanDays;
+    }
+
+    public void setMaxLoanDays(int maxLoanDays) {
+        this.maxLoanDays = maxLoanDays;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return maxLoanDays == book.maxLoanDays && Objects.equals(bookId, book.bookId) && Objects.equals(isbn, book.isbn) && Objects.equals(title, book.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bookId, isbn, title, maxLoanDays);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "bookId=" + bookId +
+                ", isbn='" + isbn + '\'' +
+                ", title='" + title + '\'' +
+                ", maxLoanDays=" + maxLoanDays +
+                '}';
+    }
+}
---
+++ src/main/java/se/lexicon/almgru/jpa_workshop/entity/BookLoan.java
@@ -0,0 +1,103 @@
+package se.lexicon.almgru.jpa_workshop.entity;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
+public class BookLoan {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer loanId;
+
+    private LocalDate loanDate;
+    private LocalDate dueDate;
+    private boolean returned;
+
+    @ManyToOne
+    @JoinColumn(name = "borrower_id", table = "book_loan")
+    private AppUser borrower;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id", table = "book_loan")
+    private Book book;
+
+    public BookLoan(Integer loanId, LocalDate loanDate, LocalDate dueDate, boolean returned, AppUser borrower, Book book) {
+        this.loanId = loanId;
+        this.loanDate = loanDate;
+        this.dueDate = dueDate;
+        this.returned = returned;
+        this.borrower = borrower;
+        this.book = book;
+    }
+
+    public BookLoan() {
+    }
+
+    public Integer getLoanId() {
+        return loanId;
+    }
+
+    public LocalDate getLoanDate() {
+        return loanDate;
+    }
+
+    public void setLoanDate(LocalDate loanDate) {
+        this.loanDate = loanDate;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public boolean isReturned() {
+        return returned;
+    }
+
+    public void setReturned(boolean returned) {
+        this.returned = returned;
+    }
+
+    public AppUser getBorrower() {
+        return borrower;
+    }
+
+    public void setBorrower(AppUser borrower) {
+        this.borrower = borrower;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BookLoan bookLoan = (BookLoan) o;
+        return returned == bookLoan.returned && Objects.equals(loanId, bookLoan.loanId) && Objects.equals(loanDate, bookLoan.loanDate) && Objects.equals(dueDate, bookLoan.dueDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(loanId, loanDate, dueDate, returned);
+    }
+
+    @Override
+    public String toString() {
+        return "BookLoan{" +
+                "loanId=" + loanId +
+                ", loanDate=" + loanDate +
+                ", dueDate=" + dueDate +
+                ", returned=" + returned +
+                '}';
+    }
+}
